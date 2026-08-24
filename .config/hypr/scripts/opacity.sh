c03e8ae92c20dbd078feb1935c9ebc5ddc68701c
43PR <your-email@example.com>
2026-08-23 23:23:36 -0700

    Update

--- .config/hypr/scripts/opacity.sh
+++ .config/hypr/scripts/opacity.sh
@@ -13,6 +13,6 @@ case "$choice" in
     *) exit 0 ;;
 esac
 
-sed -i "s/^local window_opacity = .*/local window_opacity = $opacity/" ~/.config/hypr/rules.lua
+sed -i "0,/opacity = \".* override\"/s//opacity = \"$opacity override\"/" ~/.config/hypr/rules.lua
 
 hyprctl reload
